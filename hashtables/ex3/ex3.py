d8f550dcee1f1ed65ce4be6f73022196ddb7e20a
smv5047 <smv5047@gmail.com>
2020-08-16 21:09:34 -0400

    completed has negatives, working on intersection

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,15 +3,52 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    # create dictionary
+    intersect_dict = {}
+    combined_array = []
+    for array in arrays:
+        combined_array = combined_array+array
+    # determine how many lists are being compare
+    array_volume = len(arrays)
+    # enter #s into dictionary for each list
 
-    return result
+    for num in combined_array:
+        # print(num)
+        if num in intersect_dict:
+            # if number exists add +1 to value
+            intersect_dict[num] += 1
+        else:
+            intersect_dict[num] = 1
+    # print(intersect_dict)
+    result = []
+    print(array_volume)
+
+    # results = [values == array_volume for values in intersect_dict.values()]
+    for values == array_volume in intersect_dict.values():
+        result.append(intersect_dict)
+
+    print(results)
+    # squares = [x**2 for x in range(10)]
+    # determine which keys have a value that matches the volume of lists
+    # return list of those values
+
+    # for num in arrays[0]:
+
+    # return result
 
 
-if __name__ == "__main__":
-    arrays = []
+# if __name__ == "__main__":
+#     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+#     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+#     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+#     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+#     print(intersection(arrays))
+
+array1 = [1, 2, 3, 4, 5]
+array2 = [12, 7, 2, 9, 1]
+array3 = [99, 2, 7, 1]
+
+combined_array = [array1, array2, array3]
+intersection(combined_array)
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,8 +2,20 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    # create dict
+    num_lookup = {}
+    # create resulting list
+    result = []
+    # add all numbers to hash table
+    for num in a:
+        num_lookup[num] = num
+    # search for negative numbers
+    # determine if their absolute value is within hash table
+    for num in a:
+        if num < 0:
+            # if absolute valiue is in the hash table add to results
+            if abs(num) in num_lookup:
+                result.append(abs(num))
     return result
 
 

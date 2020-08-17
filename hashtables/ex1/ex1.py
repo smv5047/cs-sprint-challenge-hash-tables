65a749469d996d491797cf07e4b0fdec2b23ec2f
smv5047 <smv5047@gmail.com>
2020-08-16 21:58:30 -0400

    cleaned up example 1 and completed stretch for example 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,25 +1,23 @@
 def get_indices_of_item_weights(weights, length, limit):
     # create dict
     weight_location = {}
-    # search through weight list
-    # create entries in weight location by using each weight as key,
-    # and placement in list as value
+    # edge cases
     if length == 2:
         if weights[0]+weights[1] == limit:
             return (1, 0)
-
+    # search through weight list
+    # create entries in weight location by using each weight as key,
+    # and placement in list as value
     for i in range(length):
         weight_location[weights[i]] = i
-
-    for key in weight_location:
-        weight_needed = limit-key
-        if weight_needed in weight_location:
-            print(weight_location[limit-key], weight_location[key])
-            return((weight_location[limit-key], weight_location[key]))
 
     # determine two keys in dict (in order) that equal limit
     # take the values of those two keys and add their values to a tuple
     # return tuple
+    for key in weight_location:
+        weight_needed = limit-key
+        if weight_needed in weight_location:
+            return((weight_location[limit-key], weight_location[key]))
 
     return None
 
